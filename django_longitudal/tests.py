b15a4aabfb6e5e50e727d27537d9437490867101
Theo van den Bogaart <todorus@todorus.com>
2014-07-28 19:22:16 +0200

    basic unittest

--- django_longitudal/tests.py
+++ django_longitudal/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from django_longitudal.views import home
 
 # Create your tests here.
@@ -9,3 +11,11 @@ class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve("/")
 		self.assertEqual(found.func, home)
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home(request)
+
+		self.assertTrue(response.content.startswith(b"<html>"))
+		self.assertIn(b"<title>Variables</title>",response.content)
+		self.assertTrue(response.content.endswith(b"</html>"))
--- django_longitudal/views.py
+++ django_longitudal/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
-def home():
-	pass
+def home(request):
+	return HttpResponse("<html><head><title>Variables</title></head><body></body></html>")
