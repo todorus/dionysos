c9fb17e52a3abbb8b56003d069db5abdc889a337
Theo van den Bogaart <todorus@todorus.com>
2014-07-28 20:31:41 +0200

    DataPoint model

--- django_longitudinal/models.py
+++ django_longitudinal/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class DataPoint(models.Model):
+	label = models.CharField(max_length=200)
+	quantity = models.CharField(max_length=200)
+	unit = models.CharField(max_length=50)
--- django_longitudinal/tests.py
+++ django_longitudinal/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 
 from django_longitudinal.views import home
+from django_longitudinal.models import DataPoint
 
 # Create your tests here.
 
@@ -19,3 +20,32 @@ class HomePageTest(TestCase):
 		self.assertTrue(response.content.startswith(b"<html>"))
 		self.assertIn(b"<title>Variables</title>",response.content)
 		self.assertTrue(response.content.endswith(b"</html>"))
+
+class DataPointTest(TestCase):
+
+	def test_saving_and_retrieving_datapoints(self):
+		item1 = DataPoint()
+		item1.label = "Inside temperature"
+		item1.quantity = "temperature"
+		item1.unit = "°C"
+		item1.save()
+
+		item2 = DataPoint()
+		item2.label = "Outside light"
+		item2.quantity = "Luminance"
+		item2.unit = "cd/m^2"
+		item2.save()
+
+		saved_items = DataPoint.objects.all()
+		self.assertEqual(saved_items.count(),2)
+
+		saved1 = saved_items[0]
+		saved2 = saved_items[1]
+
+		self.assertEqual(saved1.label, item1.label)
+		self.assertEqual(saved1.quantity, item1.quantity)
+		self.assertEqual(saved1.unit, item1.unit)
+
+		self.assertEqual(saved2.label, item2.label)
+		self.assertEqual(saved2.quantity, item2.quantity)
+		self.assertEqual(saved2.unit, item2.unit)
