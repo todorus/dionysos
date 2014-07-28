8b6fe8c306f5e4d2139c8da54298bee5bd1a14e6
Theo van den Bogaart <todorus@todorus.com>
2014-07-28 18:48:36 +0200

    Django startup test passes

---
+++ functional_tests.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
