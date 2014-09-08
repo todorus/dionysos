adaf9c5959db7a6a169ae6eb71c27359b1ab18a7
Theo van den Bogaart <todorus@todorus.com>
2014-09-08 21:54:51 +0200

    ember initalizes

---
+++ webadmin/static/js/app.js
@@ -0,0 +1,13 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
+
+console.log("reached app");
